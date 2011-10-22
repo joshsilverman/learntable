2f8d1b22744b7fcc6be5a4f9aad0d825684c7dd5
Jason Urton <jason.urton@gmail.com>
2011-10-22 01:00:19 -0400

    remove sqlite

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Question.create(name: 'Who coined the term \'lean startup\'?', 
+    answer: 'Eric Ries', 
+    first_wrong_answer: 'Clayton Christensen', 
+    second_wrong_answe: 'David Heinemeier Hansson',
+    third_wrong_answer: 'Brant Cooper'
+)
+
+Question.create(name: 'Which term refers to a fast and quantitative market testing of a product or product feature?', 
+    answer: 'Minimum viable product', 
+    first_wrong_answer: 'Maximum viable product',
+    second_wrong_answe: 'Main viable product',
+    third_wrong_answer: 'Minimum visible product'
+)
+
+Question.create(name: 'Which is an innovation that helps create a new market and value network, eventually displacing an earlier technology?', 
+    answer: 'Disruptive technology', 
+    first_wrong_answer: 'Sustaining technology',
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
+
+Question.create(name: '', 
+    answer: '', 
+    first_wrong_answer: '',  
+    second_wrong_answe: '',
+    third_wrong_answer: ''
+)
