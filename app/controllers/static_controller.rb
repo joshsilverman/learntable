206b9f1f95162d777f474fe070faa41500431e6b
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-10-21 23:31:28 -0400

    room css

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,11 @@
 class StaticController < ApplicationController
 
   def home
+    current_user = true
+    if current_user
+      render 'static/room'
+    else
+      render 'static/home'
+    end
   end
 end
