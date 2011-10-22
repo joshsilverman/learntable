1a395f76a72aa2130f7e8c99605c1281159cd788
ysilver <ysilver@ysilver-Studio-1535.(none)>
2011-10-22 14:42:28 -0400

    new facebook key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '275341545839773', 'd5ab23cf902cbdbb7aa27c86c901d03d'
+#  provider :facebook, '130026253769175', '086c42803d07d5c4dc70b4a937b99fee'
 end
